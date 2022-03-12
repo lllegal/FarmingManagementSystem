73fd51c44554dd04022af70c70ea7395a089d856
lllegal <lynchr7237@gamil.com>
2022-03-12 20:13:00 +0900

    completed menu

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,11 +1,11 @@
 
 public class MenuManager {
+	
 	public static void main(String[] args) {
-		System.out.println("1. ");
-		System.out.println("1. ");
-		System.out.println("1. ");
-		System.out.println("1. a");
-		System.out.println("1. a");
-		System.out.println("1. a");
+		System.out.println("1. Add Crops");
+		System.out.println("2. Delete Crops");
+		System.out.println("3. Edit Crops");
+		System.out.println("4. View Crops");
+		System.out.println("5. Exit");
 	}
 }
