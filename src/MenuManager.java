85f415aacf0a76d1838e000698cb0a2e9f91d31a
lllegal <lynchr7237@gamil.com>
2022-03-12 19:48:19 +0900

    first work

---
+++ src/MenuManager.java
@@ -0,0 +1,11 @@
+
+public class MenuManager {
+	public static void main(String[] args) {
+		System.out.println("1. ");
+		System.out.println("1. ");
+		System.out.println("1. ");
+		System.out.println("1. ");
+		System.out.println("1. ");
+		System.out.println("1. ");
+	}
+}
